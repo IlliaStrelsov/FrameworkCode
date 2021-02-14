570f46d5b075680a2d9bcca96a682f94a245bab7
illia <illyuha31@gmail>
2021-02-14 15:15:10 +0200

    Testing

---
+++ core/Router.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    public Request $request;
+    protected array $routes =[];
+
+    /**
+     * Router constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    public function get($path,$callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if($callback ===false){
+            return "not found";
+        }
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+
+    }
+    public function renderView(){
+
+    }
+}
